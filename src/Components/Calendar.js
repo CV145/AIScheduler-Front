3ab23d7130c8a48a2a91a9b79abba087d62ba0e9
Carlos Valeriano <carlosvaleriano45@yahoo.com>
2023-12-19 17:44:45 -0600

    Updated calendar to show tomorrow's schedule only

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -2,40 +2,52 @@ import React, { useState, useEffect } from 'react';
 import { useGoogleAuth } from '../Contexts/GoogleAuthContext';
 
 const CalendarComponent = () => {
+  const { googleToken } = useGoogleAuth();
   const [calendarEvents, setCalendarEvents] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  /*
-  Use the Google Token for API requests. Stored in the GoogleAuth Context that wraps this component
-  */
-  const {googleToken, isSignedIn} = useGoogleAuth();
+  // Function to get start and end times for tomorrow
+  const getTomorrowDates = () => {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
 
-  useEffect(() => {
-    const fetchCalendarData = async () => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const response = await fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
-          headers: {
-            Authorization: `Bearer ${googleToken}`
-          }
-        });
-        if (!response.ok) {
-          throw new Error('Failed to fetch calendar data');
+    const start = new Date(tomorrow.setHours(0, 0, 0, 0)).toISOString();
+    const end = new Date(tomorrow.setHours(23, 59, 59, 999)).toISOString();
+
+    return { start, end };
+  };
+
+  // Function to fetch calendar data
+  const fetchCalendarData = async () => {
+    if (!googleToken) return; // Ensure token is available
+
+    const { start, end } = getTomorrowDates();
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(`https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=${start}&timeMax=${end}`, {
+        headers: {
+          Authorization: `Bearer ${googleToken}`
         }
-        const data = await response.json();
-        setCalendarEvents(data.items); // Assuming 'items' contains the events
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+      });
 
-    if (googleToken) {
-      fetchCalendarData();
+      if (!response.ok) {
+        throw new Error('Failed to fetch calendar data');
+      }
+      const data = await response.json();
+      setCalendarEvents(data.items); // Assuming 'items' contains the events
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
     }
+  };
+
+  // Fetch data when component mounts or googleToken changes
+  useEffect(() => {
+    fetchCalendarData();
   }, [googleToken]);
 
   if (isLoading) {
@@ -48,12 +60,14 @@ const CalendarComponent = () => {
 
   return (
     <div>
-      <h2>Your Calendar Events</h2>
+      <h2>Tomorrow's Calendar Events</h2>
       <ul>
         {calendarEvents.map(event => (
-            <li key={event.id}>
-                {event.summary} - {event.start?.dateTime ? new Date(event.start.dateTime).toLocaleString() : 'No start time'}
-            </li>
+          <li key={event.id}>
+            {event.summary} - {event.start.dateTime ? 
+              `${new Date(event.start.dateTime).toLocaleTimeString()} - ${new Date(event.end.dateTime).toLocaleTimeString()}` : 
+              'All day'}
+          </li>
         ))}
       </ul>
     </div>
